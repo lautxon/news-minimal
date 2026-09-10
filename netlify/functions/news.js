5e1f5490d344d63161dc60c630c1a1f699faba80
lautxon <125925421+lautxon@users.noreply.github.com>
2026-09-10 13:36:12 -0300

    Increase item limit from 10 to 25 in news function

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -33,7 +33,7 @@ function parseRSS(xmlText, source) {
   let match;
   let count = 0;
   
-  while ((match = itemRegex.exec(xmlText)) !== null && count < 10) {
+  while ((match = itemRegex.exec(xmlText)) !== null && count < 25) {
     const item = match[1];
     
     const getTag = (tag) => {
